711310c9961b223d7fcca84a37171f33908945f7
rhon-ux <rhon@letsgetfunded.com>
2026-07-01 00:05:21 +0800

    Delete src directory

--- src/components/admin/Notification.jsx
+++
@@ -1,9 +0,0 @@
-export default function Notification({ notification }) {
-  if (!notification) return null;
-
-  return (
-    <div className={`notification notification--${notification.type}`}>
-      <span>{notification.type === "error" ? "✕" : "✓"}</span> {notification.msg}
-    </div>
-  );
-}
